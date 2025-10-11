a26c30386ada3734d8c4cdf8b75410d5adf429b6
Manish Sharma <manishsharma@Manishs-MacBook-Air.local>
2025-10-11 15:54:19 +0530

    mobile view

--- js/main.js
+++ js/main.js
@@ -10,22 +10,22 @@
         }, 1);
     };
     spinner();
-    
-    
+
+
     // Initiate the wowjs
     new WOW().init();
 
 
-    // Sticky Navbar
-    $(window).scroll(function () {
+    // Sticky Navbar: toggle shadow only. Keep navbar at top (CSS handles positioning).
+    $(window).on('scroll', function () {
         if ($(this).scrollTop() > 300) {
-            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
+            $('.sticky-top').addClass('shadow-sm');
         } else {
-            $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
+            $('.sticky-top').removeClass('shadow-sm');
         }
     });
-    
-    
+
+
     // Back to top button
     $(window).scroll(function () {
         if ($(this).scrollTop() > 300) {
@@ -35,7 +35,7 @@
         }
     });
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
         return false;
     });
 
@@ -49,6 +49,6 @@
         loop: true,
     });
 
-    
+
 })(jQuery);
 
